bc03ff3d73dee15bdda4d9982674cc98f38ed45a
Leigh Klotz <klotz@klotz.me>
2021-01-25 22:01:16 -0800

    move count-words to text-hacks

--- klotz/klotz.emacs
+++ klotz/klotz.emacs
@@ -180,15 +180,6 @@
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 ;(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 
-
-
-(defun count-words (&optional begin end)
-  "count words between BEGIN and END (region); if no region defined, count words in buffer"
-  (interactive "r")
-  (let ((b (if mark-active begin (point-min)))
-      (e (if mark-active end (point-max))))
-    (message "Word count: %s" (how-many "\\w+" b e))))
-
 ;;; Raman's Google Hacks
 ;;; (push "~klotz/emacs/g-client/" load-path)
 ;;; (load-library "g")
--- klotz/text-hacks.el
+++ klotz/text-hacks.el
@@ -124,6 +124,14 @@ selected rectangle."
 "
 ")))
 
+;; I often want the whole buffer, and this makes the short name work.
+(defun count-words (&optional begin end)
+  "count words between BEGIN and END (region); if no region defined, count words in buffer"
+  (interactive "r")
+  (let ((b (if mark-active begin (point-min)))
+      (e (if mark-active end (point-max))))
+    (message "Word count: %s" (how-many "\\w+" b e))))
+
 ;; Always end a file with a newline
 (setq require-final-newline 'ask)
 ;; Stop at the end of the file, not just add lines
