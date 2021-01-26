24935b32a8f319904f95914574b9ead7eed06460
leighklotz <klotz@klotz.me>
2021-01-25 21:02:14 -0800

    Move existing custom settings to custom-core.el

---
+++ klotz/custom-core.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(isearch-lax-whitespace nil t)
+ '(nodemcu-debug-io t)
+ '(nodemcu-interface (quote nodemcu-interface-serial))
+ '(nodemcu-target-device "/dev/ttyUSB1")
+ '(package-selected-packages
+   (quote
+   ;; php+-mode
+   ;; flycheck
+    (json-mode ham-mode emojify aggressive-indent ack)))
+ '(safe-local-variable-values (quote ((backup-inhibited . t)))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-string-face ((((class color) (background light)) (:foreground "Blue")))))
