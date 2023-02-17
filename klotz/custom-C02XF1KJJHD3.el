3b4832d5fd8e46cb915e63066deff78f6c4ed2af
Leigh Klotz <klotz@yahooinc.com>
2023-02-17 12:36:00 -0800

    updates

--- klotz/custom-C02XF1KJJHD3.el
+++ klotz/custom-C02XF1KJJHD3.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ack string-inflection yaml-mode tldr scala-mode pig-mode php-mode markdown-mode json-mode graphviz-dot-mode go-mode flymake-json exec-path-from-shell csv-mode csv company)))
+   '(ace-window ack string-inflection yaml-mode tldr scala-mode pig-mode php-mode markdown-mode json-mode graphviz-dot-mode go-mode flymake-json exec-path-from-shell csv-mode csv company)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
